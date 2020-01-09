1cd40c140e5c15a8e879e8b97dbd46eb98f96963
sne <snezhana.syngur@gmail.com>
2020-01-09 22:13:49 +0100

    Add colors-properties to the color.scss, remove blockquote element

--- anadea-new/gulpfile.js
+++ anadea-new/gulpfile.js
@@ -4,12 +4,14 @@ var gulp = require('gulp');
 var del = require('del');
 var plumber = require('gulp-plumber');
 var sourcemap = require('gulp-sourcemaps');
+var server = require("browser-sync").create();
 var rename = require('gulp-rename');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var csso = require('gulp-csso');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
+// var svgstore = require('svgstore');
 
 gulp.task('message', function (done) {
     done();
@@ -19,7 +21,6 @@ gulp.task('message', function (done) {
 gulp.task('clean', function () {
     return del('build');
 });
-
 
 gulp.task('copy', function () {
     return gulp.src([
@@ -46,6 +47,15 @@ gulp.task('imageMin', function (done) {
         .pipe(gulp.dest('build/img'));
     done();
 });
+
+// gulp.task('sprite', function () {
+//   return gulp.src('source/img/sprite-*.svg')
+//     .pipe(svgstore({
+//       inlineSvg: true
+//     }))
+//     .pipe(rename('sprite.svg'))
+//     .pipe(gulp.dest('build/img'));
+// });
 
 gulp.task('css', function () {
     return gulp.src('source/sass/style.scss')
