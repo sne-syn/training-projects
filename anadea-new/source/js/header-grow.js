15ed7d3724b24db1306f935e2a0f5660b6f540a6
sne <snezhana.syngur@gmail.com>
2020-01-13 14:00:05 +0100

    Adaptive width changes

--- anadea-new/source/js/header-grow.js
+++ anadea-new/source/js/header-grow.js
@@ -1,32 +1,40 @@
 'use strict';
 
 (function () {
-   var X_SMALL_SCREEN = window.matchMedia("(max-width: 765px)");
+    var X_SMALL_SCREEN = window.matchMedia("(max-width: 765px)");
     var SMALL_SCREEN = window.matchMedia("(min-width: 766px)");
     var LARGE_SCREEN = window.matchMedia("(min-width: 1199px)");
 
     var changeLogoOnScroll = function () {
-        var header = document.querySelector('header__block');
-        var headerLogo = document.querySelector('.header__logo-block');
-        var icon = headerLogo.querySelector('.header__logo-icon');
-        var nav = document.querySelector('.nav');
+        var header = document.querySelector('.header__block');
+        var headerLogo = header.querySelector('.header__logo-block');
+        var icon = header.querySelector('.header__logo-icon');
+        var nav = header.querySelector('.nav');
         var yCoord = window.pageYOffset;
 
         if (yCoord < 100 && SMALL_SCREEN.matches) {
             icon.style = "display: block";
+            header.style = "margin-top: 0; margin-bottom: 0;";
         }
         if (yCoord < 100 && LARGE_SCREEN.matches) {
             headerLogo.style = 'padding: 84px 20px 23px';
             icon.style = "display: block";
             nav.style = "margin-top: 35px";
+            header.style = "margin-top: 0; margin-bottom: 0;";
         }
-        if (yCoord > 100 || X_SMALL_SCREEN.matches) {
+        if (yCoord < 100 && X_SMALL_SCREEN.matches) {
+            headerLogo.style = "padding: 9px 0 21px";
+            icon.style = "display: none;";
+            nav.style = "margin: 0";
+            header.style = "margin-top: 0; margin-bottom: 0;";
+        }
+        if (yCoord > 100) {
             headerLogo.style = "padding: 9px 0 21px";
             icon.style = "display: none;";
             nav.style = "margin: 0";
             header.style = "margin-top: 20px; margin-bottom: 20px;";
         }
     };
-    
+
     window.addEventListener('scroll', changeLogoOnScroll);
 })();
